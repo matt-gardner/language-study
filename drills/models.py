e1f56a337d64385240b1cc3231ac1c51ab715af5
Matthew Gardner <matt@matthew-gardners-computer.local>
2010-12-27 12:21:44 -0700

    Set up the database to be able to handle the new language drills

--- drills/models.py
+++ drills/models.py
@@ -2,7 +2,79 @@ from django.db import models
 
 from datetime import datetime
 
-class CardList(models.Model):
+
+# Language Models
+#################
+
+class Language(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Declension(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Case(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Number(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Gender(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Conjugation(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Tense(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Voice(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Mood(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+class DeclinableType(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey('Language')
+
+
+# Drilling Models
+#################
+
+# We have some redundancy in the way this is set up (i.e., a foreign key that
+# could be retrieved by just doing a join on a key that's already present, like
+# ConjugationDrillResult pointing to both the verb and the wordlist), but I
+# figured that the easier lookups and aggregrations would be worth having a
+# little redundancy, because we will be careful to only modify the database in
+# valid ways.
+
+
+class WordList(models.Model):
     user = models.ForeignKey('auth.User')
     name = models.CharField(max_length=128)
 
@@ -10,14 +82,25 @@ class CardList(models.Model):
         return u'%s: %s' % (self.user.first_name, self.name)
 
 
-class Card(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=128)
+    wordlist = models.ForeignKey('WordList')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Word(models.Model):
     DIFFICULTY_SCORES = {
             'easy': 1,
             'medium': 20,
             'hard': 40,
             }
     DIFFICULTY_ALPHA = .4
-    list = models.ForeignKey('CardList')
+    wordlist = models.ForeignKey('WordList')
     word = models.CharField(max_length=128)
     text = models.CharField(max_length=4096)
     last_reviewed = models.DateTimeField()
@@ -28,12 +111,12 @@ class Card(models.Model):
 
     def update_difficulty(self, score):
         old = self.average_difficulty
-        alpha = Card.DIFFICULTY_ALPHA
+        alpha = Word.DIFFICULTY_ALPHA
         new = (1 - alpha) * self.average_difficulty + alpha * score
         self.average_difficulty = new
         self.save()
         difference = new - old
-        self.list.save()
+        self.wordlist.save()
 
     def reviewed(self):
         self.last_reviewed = datetime.now()
@@ -41,18 +124,41 @@ class Card(models.Model):
         self.save()
 
     def __unicode__(self):
-        return u'%s: %s' % (self.list.name, self.word)
+        return u'%s: %s' % (self.wordlist.name, self.word)
 
 
-class Tag(models.Model):
-    name = models.CharField(max_length=128)
-    list = models.ForeignKey('CardList')
+class Verb(Word):
+    conjugation = models.ForeignKey('Conjugation')
 
-    def __unicode__(self):
-        return self.name
 
-    class Meta:
-        ordering = ['name']
+class DeclinableWord(Word):
+    declension = models.ForeignKey('Declension')
+    type = models.ForeignKey('DeclinableType')
+
+
+class ConjugationDrillResult(models.Model):
+    wordlist = models.ForeignKey('WordList')
+    verb = models.ForeignKey('Verb')
+    conjugation = models.ForeignKey('Conjugation')
+    person = models.ForeignKey('Person')
+    number = models.ForeignKey('Number')
+    tense = models.ForeignKey('Tense')
+    mood = models.ForeignKey('Mood')
+    voice = models.ForeignKey('Voice')
+    correct = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class DeclensionDrillResult(models.Model):
+    wordlist = models.ForeignKey('WordList')
+    word = models.ForeignKey('DeclinableWord')
+    type = models.ForeignKey('DeclinableType')
+    declension = models.ForeignKey('Declension')
+    gender = models.ForeignKey('Gender')
+    number = models.ForeignKey('Number')
+    case = models.ForeignKey('Case')
+    correct = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
 
 
 # vim: et sts=4 sw=4
