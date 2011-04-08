401537e1e3b8e72349674bd3b1dc85ddf2841e09
Matt Gardner <mjg82@byu.edu>
2011-04-07 22:58:20 -0600

    Trying to improve add_to_db
    In particular, I would like to make the process of adding irregular
    forms for testing easier than it has been, and also make it persistent
    so that when I want to make a default database, it is nice and easy.  So
    I've restructured add_to_db.py a bit to make it handle adding words and
    irregular forms and all of that.  As I've been doing this, I think a
    smarter thing would be to have a json file or set of json files
    describing all of these objects, then load that json file to add things
    to the database.  That would be easier than writing it in python.  But
    at least this is a start.

--- attic_greek/add_to_db.py
+++ attic_greek/add_to_db.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
 import os, sys
 
 sys.path.append(os.getcwd()+'/..')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'language_study.settings'
 
+from collections import defaultdict
+from copy import deepcopy
+from datetime import datetime
+from django.contrib.auth.models import User
 from language_study.drills.models import *
 from optparse import OptionParser
 
-def add_language(incremental=False):
+incremental = False
+added = []
+object_cache = defaultdict(dict)
+
+def add_language():
     language_name = 'Attic Greek'
     module_name = 'attic_greek'
-    added = []
     try:
         language = Language.objects.get(name=language_name)
         if not incremental:
@@ -20,67 +28,227 @@ def add_language(incremental=False):
         added.append('Language: ' + language_name)
         language = Language(name=language_name, module_name=module_name)
         language.save()
+    object_cache['Language'][language_name] = language
+
     # Declensions
-    add_or_fail('First Declension', Declension, language, incremental, added)
-    add_or_fail('Second Declension', Declension, language, incremental, added)
-    add_or_fail('Third Declension', Declension, language, incremental, added)
+    args = {'name': 'First Declension', 'language': language}
+    add_or_fail(Declension, args)
+    args['name'] = 'Second Declension'
+    add_or_fail(Declension, args)
+    args['name'] = 'Third Declension'
+    add_or_fail(Declension, args)
+
     # Cases
-    add_or_fail('Nominative', Case, language, incremental, added)
-    add_or_fail('Genitive', Case, language, incremental, added)
-    add_or_fail('Dative', Case, language, incremental, added)
-    add_or_fail('Accusative', Case, language, incremental, added)
-    add_or_fail('Vocative', Case, language, incremental, added)
+    args['name'] = 'Nominative'
+    add_or_fail(Number, args)
+    args['name'] = 'Genitive'
+    add_or_fail(Number, args)
+    args['name'] = 'Dative'
+    add_or_fail(Number, args)
+    args['name'] = 'Accusative'
+    add_or_fail(Number, args)
+    args['name'] = 'Vocative'
+    add_or_fail(Number, args)
+
     # Numbers
-    add_or_fail('Singular', Number, language, incremental, added)
-    add_or_fail('Plural', Number, language, incremental, added)
-    add_or_fail('None', Number, language, incremental, added)
+    args['name'] = 'Singular'
+    add_or_fail(Number, args)
+    args['name'] = 'Plural'
+    add_or_fail(Number, args)
+    args['name'] = 'None'
+    add_or_fail(Number, args)
+
     # Genders
-    add_or_fail('Masculine', Gender, language, incremental, added)
-    add_or_fail('Feminine', Gender, language, incremental, added)
-    add_or_fail('Neuter', Gender, language, incremental, added)
+    args['name'] = 'Masculine'
+    add_or_fail(Gender, args)
+    args['name'] = 'Feminine'
+    add_or_fail(Gender, args)
+    args['name'] = 'Neuter'
+    add_or_fail(Gender, args)
+
     # DeclinableTypes
-    add_or_fail('Noun', DeclinableType, language, incremental, added)
-    add_or_fail('Pronoun', DeclinableType, language, incremental, added)
-    add_or_fail('Adjective', DeclinableType, language, incremental, added)
+    args['name'] = 'Noun'
+    add_or_fail(DeclinableType, args)
+    args['name'] = 'Pronoun'
+    add_or_fail(DeclinableType, args)
+    args['name'] = 'Adjective'
+    add_or_fail(DeclinableType, args)
+
     # Conjugations
-    add_or_fail('Thematic', Conjugation, language, incremental, added)
+    args['name'] = 'Thematic'
+    add_or_fail(Conjugation, args)
+    args['name'] = 'Athematic'
+    add_or_fail(Conjugation, args)
+
     # Persons
-    add_or_fail('First Person', Person, language, incremental, added)
-    add_or_fail('Second Person', Person, language, incremental, added)
-    add_or_fail('Third Person', Person, language, incremental, added)
-    add_or_fail('None', Person, language, incremental, added)
+    args['name'] = 'First Person'
+    add_or_fail(Person, args)
+    args['name'] = 'Second Person'
+    add_or_fail(Person, args)
+    args['name'] = 'Third Person'
+    add_or_fail(Person, args)
+    args['name'] = 'None'
+    add_or_fail(Person, args)
+
     # Tenses
-    add_or_fail('Present', Tense, language, incremental, added)
-    add_or_fail('Imperfect', Tense, language, incremental, added)
-    add_or_fail('Future', Tense, language, incremental, added)
-    add_or_fail('Aorist', Tense, language, incremental, added)
-    add_or_fail('Perfect', Tense, language, incremental, added)
-    add_or_fail('Pluperfect', Tense, language, incremental, added)
+    args['name'] = 'Present'
+    add_or_fail(Tense, args)
+    args['name'] = 'Imperfect'
+    add_or_fail(Tense, args)
+    args['name'] = 'Future'
+    add_or_fail(Tense, args)
+    args['name'] = 'Aorist'
+    add_or_fail(Tense, args)
+    args['name'] = 'Perfect'
+    add_or_fail(Tense, args)
+    args['name'] = 'Pluperfect'
+    add_or_fail(Tense, args)
+
     # Voices
-    add_or_fail('Active', Voice, language, incremental, added)
-    add_or_fail('Middle', Voice, language, incremental, added)
-    add_or_fail('Passive', Voice, language, incremental, added)
+    args['name'] = 'Active'
+    add_or_fail(Voice, args)
+    args['name'] = 'Middle'
+    add_or_fail(Voice, args)
+    args['name'] = 'Passive'
+    add_or_fail(Voice, args)
+
     # Moods
-    add_or_fail('Indicative', Mood, language, incremental, added)
-    add_or_fail('Subjunctive', Mood, language, incremental, added)
-    add_or_fail('Optative', Mood, language, incremental, added)
-    add_or_fail('Imperative', Mood, language, incremental, added)
-    add_or_fail('Infinitive', Mood, language, incremental, added)
+    args['name'] = 'Indicative'
+    add_or_fail(Mood, args)
+    args['name'] = 'Subjunctive'
+    add_or_fail(Mood, args)
+    args['name'] = 'Optative'
+    add_or_fail(Mood, args)
+    args['name'] = 'Imperative'
+    add_or_fail(Mood, args)
+    args['name'] = 'Infinitive'
+    add_or_fail(Mood, args)
+
+    # Default WordList
+    args['name'] = 'Attic Greek Default Words'
+    # TODO: make this a little better...  Maybe make a default user
+    args['user'] = User.objects.get(pk=1)
+    add_or_fail(WordList, args)
+
+    # Words
+    # Thematic verbs
+    args = {'wordlist': object_cache['WordList'][args['name']]}
+    args['conjugation'] = object_cache['Conjugation']['Thematic']
+    args['word'] = u'ἀγγέλλω, ἀγγελῶ, ἤγγειλα, ἤγγελκα, ἤγγελμαι, ἠγγέλθην'
+    args['definition'] = u'to announce'
+    create_args = {'last_reviewed': datetime.now()}
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'αἰσχύνομαι, αἰσχυνοῦμαι, _, _, ᾔσχυμμαι, ᾐσχύνθην'
+    args['definition'] = u'to be ashamed'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'δηλόω, δηλώσω, ἐδήλωσα, δεδήλωκα, δεδήλωμαι, ἐδηλώθην'
+    args['definition'] = u'to make clear'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'ἐλαύνω, ἐλάω, ἤλασα, ἐλήλακα, ἐλήλαμαι, ἠλάθην'
+    args['definition'] = u'to drive, march'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'ἐλέγχω, ἐλέγξω, ἤλεγξα, _, ἐλήλεγμαι, ἠλέγχθην'
+    args['definition'] = u"I'm not sure..."
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'ἔρχομαι, ἐλεύσομαι, ἦλθον, ἐλήλυθα, _, _'
+    args['definition'] = u'to come'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'ἐθέλω, ἐθελήσω, ἠθέλησα, ἠθέληκα, _, _'
+    args['definition'] = u'to wish, want'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'φαίνω, φανῶ, ἔφηνα, πέφηνα, πέφασμαι, ἐφάνην'
+    args['definition'] = u'to show'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'φοβέομαι, φοβήσομαι, _, _, πεφόβημαι, ἐφοβήθην'
+    args['definition'] = u'to fear, be afraid'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'γράφω, γράψω, ἔγραψα, γέγραφα, γέγραμμαι, ἐγράφην'
+    args['definition'] = u'to write, draw'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'κελεύω, κελεύσω, ἐκέλευσα, κεκέλευκα, κεκέλευσμαι, '\
+            u'ἐκελεύσθην'
+    args['definition'] = u'to command, order'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'μάχομαι, μαχοῦμαι, ἐμαχεσάμην, _, μεμάχημαι, _'
+    args['definition'] = u'to fight'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'νικάω, νικήσω, ἐνίκησα, νενίκηκα, νενίκημαι, ἐνικήθην'
+    args['definition'] = u'to win, conquer, be victorious'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'παιδεύω, παιδεύσω, ἐπαίδευσα, πεπαίδευκα, πεπαίδευμαι, '\
+            u'ἐπαιδεύθην'
+    args['definition'] = u'to teach'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'πέμπω, πέμψω, ἔπεμψα, πέπομφα, πέπεμμαι, ἐπέμφθην'
+    args['definition'] = u'to send'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'ποιέω, ποιήσω, ἐποίησα, πεποίηκα, πεποίημαι, ἐποιήθην'
+    args['definition'] = u'to do'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'τάττω, τάξω, ἔταξα, τέταχα, τέταγμαι, ἐτάχθην'
+    args['definition'] = u'to arrange, station, align, order'
+    add_or_fail(Verb, args, create_args)
+
+    # Athematic verbs
+    irreg_args = {}
+    args['conjugation'] = object_cache['Conjugation']['Athematic']
+    args['word'] = u'δίδωμι, δώσω, ἔδωκα, δέδωκα, δέδομαι, ἐδόθην'
+    args['definition'] = u'to give'
+    add_or_fail(Verb, args, create_args)
+    irregular_verb_form(args['word'], 'First Person', 'Singular', 'Imperfect',
+            'Indicative', 'Active', u'ἐδίδουν')
+    irregular_verb_form(args['word'], 'Second Person', 'Singular', 'Imperfect',
+            'Indicative', 'Active', u'ἐδίδους')
+    irregular_verb_form(args['word'], 'Third Person', 'Singular', 'Imperfect',
+            'Indicative', 'Active', u'ἐδίδου')
+    irregular_verb_form(args['word'], 'Second Person', 'Singular', 'Present',
+            'Imperative', 'Active', u'δίδου')
+    args['word'] = u'ἵστημι, στήσω, ἔστησα, ἕστηκα, ἕσταμαι, ἐστάθην'
+    args['definition'] = u'to stand'
+    add_or_fail(Verb, args, create_args)
+    args['word'] = u'τίθημι, θήσω, ἔθηκα, τέθηκα, τέθειμαι, ἐτέθην'
+    args['definition'] = u'to put'
+    add_or_fail(Verb, args, create_args)
+
     for a in added:
         print 'Added:', a
     if not added:
         print 'Nothing added to the database'
 
 
-def add_or_fail(name, model, language, incremental, added):
+def irregular_verb_form(verb, person, number, tense, mood, voice, form):
+    args = {}
+    args['verb'] = object_cache['Verb'][verb]
+    args['person'] = object_cache['Person'][person]
+    args['number'] = object_cache['Number'][number]
+    args['tense'] = object_cache['Tense'][tense]
+    args['mood'] = object_cache['Mood'][mood]
+    args['voice'] = object_cache['Voice'][voice]
+    args['form'] = form
+    add_or_fail(IrregularVerbForm, args)
+
+
+def add_or_fail(model, query_args, create_args={}):
+    global object_cache
+    if 'name' in query_args:
+        output = query_args['name']
+    elif 'word' in query_args:
+        output = query_args['word']
+    elif 'form' in query_args:
+        output = query_args['form']
+    else:
+        output = 'Something'
     try:
-        obj = model.objects.get(name=name, language=language)
+        obj = model.objects.get(**query_args)
         if not incremental:
             raise RuntimeError('%s is already in the database!' % name)
     except model.DoesNotExist:
-        obj = model(name=name, language=language)
+        query_args.update(create_args)
+        obj = model(**query_args)
         obj.save()
-        added.append(model.__name__ + ': ' + name)
+        global added
+        added.append(model.__name__ + ': ' + output)
+    object_cache[model.__name__][output] = obj
 
 
 if __name__ == '__main__':
@@ -94,6 +262,8 @@ if __name__ == '__main__':
             'in the database.',
             )
     options, args = parser.parse_args()
-    add_language(incremental=options.incremental)
+    if options.incremental:
+        incremental = True
+    add_language()
 
 # vim: et sw=4 sts=4
