1c174f521595b9c61bcc377aeaa35302ce667fe1
Matt Gardner <mjg82@byu.edu>
2011-09-02 21:57:22 -0400

    Added Slovene as an installed app

--- settings.py
+++ settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'language_study.drills',
     'language_study.attic_greek',
+    'language_study.slovene',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
 )
